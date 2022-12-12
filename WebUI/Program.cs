82d58d27574468be6cafd7a9d0a140f1800c0ec4
Zobair Hussain <49007316+ZobairRUET@users.noreply.github.com>
2022-12-13 00:28:46 +0600

    Add project files.

---
+++ WebUI/Program.cs
@@ -0,0 +1,9 @@
+using CleanArchitecture.Application;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
+builder.Services.AddWebUIServices();
+
